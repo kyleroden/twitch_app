4f1b3aae98b3bad315305630a3cd47e592502579
kyleroden <kyleroden77@gmail.com>
2016-07-20 22:46:35 -0700

    put bootstrap divs in li items, added preview picture, cleaned up js script

--- script.js
+++ script.js
@@ -4,30 +4,60 @@ twitch_app = {};
 
 twitch_app.twitch_url = "https://api.twitch.tv/kraken/streams";
 
-twitch_app.channels = ['freecodecamp', 'noobs2ninjas', 'brtt', 'comster404'];
+twitch_app.channels = ['freecodecamp', 'noobs2ninjas', 'brtt', 'comster404', 'ambishh', 'brownman', 'pro_dota2_tv', 'thegamingowl123', 'cashnastygaming'];
 
 $.each(twitch_app.channels, function(index, channel) {
   $.getJSON(twitch_app.twitch_url + "/" + channel + "?callback=?", function(channel_data) {
-    var current_streamer = "<li>";
-    current_streamer += "<p class= 'lead'>" + channel + "</p>";
+    var current_streamer = '';
     console.log(channel_data.stream);
     if (channel_data.stream === undefined) {
-      current_streamer += "<p class='small'>" + "User no longer exists" + "</p>" + "</li>";
+      current_streamer += "<li>";
+      current_streamer += "<div class='row'>" ;
+      current_streamer += "<div class='col-md-12'>";
+      current_streamer += "<div class='col-md-8'>";
+      current_streamer += "<p class= 'lead'>" + channel + "</p>";
+      current_streamer += "<p class=''>" + "User no longer exists" + "</p>";
+      current_streamer += "</div>";
+      current_streamer += "</div>";
+      current_streamer += "</div>";
+      current_streamer += "</li>";
       $("#results_list").append(current_streamer);
-      //$("#results_list").append("<li>" + "Online" + "</li>")
     } else if (channel_data.stream === null) {
-      current_streamer += "<p class='small'>" + "Offline" + "</p>" + "</li>";
+      current_streamer += "<li>";
+      current_streamer += "<div class='row'>";
+      current_streamer += "<div class='col-md-12'>";
+      current_streamer += "<div class='col-md-8'>";
+      current_streamer += "<p class= 'lead'>" + channel + "</p>";
+      current_streamer += "<p class=''>" + "Offline" + "</p>";
+      current_streamer += "</div>";
+      current_streamer += "</div>";
+      current_streamer += "</div>";
+      current_streamer += "</li>";
       $("#results_list").append(current_streamer);
-      //$("#results_list").append("<li>" + "Offline" + "</li>")
+
     } else {
-      current_streamer += "<a target='_blank' href='" + "https://www.twitch.tv/" + channel + "'>" + "<p class='small'>" + "Online" + "</p>" + "</a>" + "</li>";
+      current_streamer += "<li>";
+      current_streamer += "<div class='row'>";
+      current_streamer += "<div class='col-md-12'>";
+      current_streamer += "<div class='col-md-8'>";
+      current_streamer += "<p class= 'lead'>" + channel + "</p>";
+      current_streamer += "<a target='_blank' href='" + "https://www.twitch.tv/" + channel + "'>" + "<p class=''>" + "Online" + "</p>" + "</a>";
+      current_streamer += "<p class=''>" + channel_data.stream.game + "</p>";
+      current_streamer += "<p>Viewers: " + channel_data.stream.viewers + "</p>";
+      current_streamer += "</div>";
+      current_streamer += "<div class='col-md-4'>";
+      current_streamer += "<img class='preview_img' src='" + channel_data.stream.preview.medium + "'>";
+      current_streamer += "</div>";
+      current_streamer += "</div>";
+      current_streamer += "</div>";
+      current_streamer += "</li>";
       $("#results_list").append(current_streamer);
     }
     console.log(channel_data);
   });
 });
-$("body").css("background-color", "cornflowerblue");
 
+/*
 $.ajax({
   type: 'GET',
   url: twitch_app.twitch_url,
@@ -37,10 +67,7 @@ $.ajax({
   crossDomain: true,
   dataType: 'jsonp',
   success: function(data) {
-    //console.log(data);
     var results = $("#results_list");
-    $("#eatshit").text(data.streams[0].average_fps);
-    //$("#eatshit").text(data.streams.channel:freecodecamp);
     $.each(data.streams, function(index, value) {
       results += '<li>';
       results += value.channel.display_name;
@@ -49,3 +76,5 @@ $.ajax({
     $("#results_list").append(results);
   }
 });
+*/
+var always_do;
